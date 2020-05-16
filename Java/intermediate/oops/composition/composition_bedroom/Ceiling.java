21c8bad71d7eed5b8f7e796aba59a1f097008d36
Shreyas Basutkar <shreyasbasutkar21@gmail.com>
2020-05-16 21:14:25 +0530

    Composition code via Java

---
+++ Java/intermediate/oops/composition/composition_bedroom/Ceiling.java
@@ -0,0 +1,22 @@
+package oops.composition_Bedroom;
+
+public class Ceiling {
+
+	private int height;
+	private int paintedColour;
+
+	public Ceiling(int height, int paintedColour) {
+		this.height = height;
+		this.paintedColour = paintedColour;
+
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getPaintedColour() {
+		return paintedColour;
+	}
+	
+}
